8adeab7da7d3e3faf0abc78cc3e23034b198bd39
blankochan <liz@blanky.dev>
2025-07-13 23:54:33 -0500

    ARRGGGG fucking change the - to a _ because thunderstore doesnt like me

--- src/BuildInfo.cs
+++ src/BuildInfo.cs
@@ -2,7 +2,7 @@
 
 public static class BuildInfo
 {
-    public const string Name = "glTF-Exporter";
+    public const string Name = "glTF_Exporter";
     public const string Author = "blankochan";
     public const string Version = "1.0.0";
 }
